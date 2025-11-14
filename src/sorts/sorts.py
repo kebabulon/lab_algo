6b7530a187bcc2fc7f4f4672ffa3fe54d8277b02
kebabulon <3dmod07@mail.ru>
2025-11-14 17:45:16 +0300

    Implement `stable` argument for `multisort` decorator, fix sort functions return type missing

--- src/sorts/sorts.py
+++ src/sorts/sorts.py
@@ -4,6 +4,7 @@ from typing import Any, TypeVar, Optional, Protocol
 
 from copy import copy
 
+
 T = TypeVar('T')
 
 KeyFunc = Callable[[T], Any]
@@ -11,62 +12,72 @@ KeyFunc = Callable[[T], Any]
 KeyType = Optional[KeyFunc]
 CmpType = Optional[Callable[[T, T], int]]
 
+SortCallable = Callable[[list[T], KeyFunc], list[T]]
 
-class SortCallable(Protocol[T]):
+
+class MultisortCallable(Protocol[T]):
     def __call__(self, a: list[T], *, key: KeyType = None, cmp: CmpType = None, reverse: bool = False) -> list[T]:
         ...
 
 
-def multisort(sort_func: Callable[[list[T], KeyFunc], list[T]]) -> SortCallable:
-    @wraps(sort_func)
-    def multisorted(a: list[T], *, key: KeyType = None, cmp: CmpType = None, reverse: bool = False) -> list[T]:
-        if not a:
-            return []
-
-        if key and cmp:
-            raise ValueError("Both key and cmp arguments are defined")
-
-        result = a
-
-        if cmp:
-            key = cmp_to_key(cmp)
-            key_len = 0
-        else:
-            if not key:
-                def key(x):
-                    return x
-
-            key_len = 0
-            keys = key(a[0])
-            if isinstance(keys, Iterable) and isinstance(keys, Sized):
-                key_len = len(keys)
-
-        # reverse, so that unsorted elements have reverse order
-        if reverse:
-            result = list(reversed(result))
-
-        if key_len:
-            # sort in reverse order to keep correct element order (stable sorting)
-            for key_index in range(key_len - 1, -1, -1):
-                result = sort_func(result, lambda x: key(x)[key_index])  # type: ignore
-        else:
-            result = sort_func(result, key)
-
-        # sort is stable, meaning unsorted elements are in reverse order
-        # reverse again, so that sorted elements have reverse order
-        # https://docs.python.org/3/howto/sorting.html#odds-and-ends
-        if reverse:
-            result = list(reversed(result))
-
-        return result
-
-    return multisorted
+sorts_dict: dict[str, MultisortCallable] = {}
 
 
-@multisort
-def bubble_sort(a: list[T], key: KeyFunc):
-    a = copy(a)
+def multisort(stable: bool) -> Callable[[SortCallable], MultisortCallable]:
+    def multisort_decorator(sort_func: SortCallable) -> MultisortCallable:
+        @wraps(sort_func)
+        def multisorted(a: list[T], *, key: KeyType = None, cmp: CmpType = None, reverse: bool = False) -> list[T]:
+            if not a:
+                return []
 
+            if key and cmp:
+                raise ValueError("Both key and cmp arguments are defined")
+
+            result = copy(a)
+
+            if cmp:
+                key = cmp_to_key(cmp)
+                key_len = 0
+            else:
+                if not key:
+                    def key(x):
+                        return x
+
+                key_len = 0
+                keys = key(a[0])
+                if isinstance(keys, Iterable) and isinstance(keys, Sized):
+                    key_len = len(keys)
+
+            # reverse, so that unsorted elements have reverse order
+            # https://docs.python.org/3/howto/sorting.html#odds-and-ends
+            if reverse and stable:
+                result = list(reversed(result))
+
+            if key_len:
+                if stable:
+                    # sort in reverse order to keep correct element order (stable sorting)
+                    for key_index in range(key_len - 1, -1, -1):
+                        result = sort_func(result, lambda x: key(x)[key_index])  # type: ignore
+                else:
+                    raise ValueError("Multiple keys are not supported for unstable sorts")
+            else:
+                result = sort_func(result, key)
+
+            # if sort is stable, unsorted elements are now in reverse order
+            # reverse again, so that sorted elements have reverse order
+            if reverse:
+                result = list(reversed(result))
+
+            return result
+
+        sorts_dict[sort_func.__name__] = multisorted
+
+        return multisorted
+    return multisort_decorator
+
+
+@multisort(stable=True)
+def bubble_sort(a: list[T], key: KeyFunc) -> list[T]:
     while True:
         swapped = False
         for i in range(1, len(a)):
@@ -79,7 +90,7 @@ def bubble_sort(a: list[T], key: KeyFunc):
     return a
 
 
-def sort_by_center(a: list[T], low: int, high: int, key: KeyFunc):
+def sort_by_center(a: list[T], low: int, high: int, key: KeyFunc) -> int:
     i = low
 
     for j in range(low, high):
@@ -100,17 +111,15 @@ def quick_sort_helper(a: list[T], low: int, high: int, key: KeyFunc):
         quick_sort_helper(a, low, center - 1, key)
 
 
-@multisort
-def quick_sort(a: list[T], key: KeyFunc):
-    a = copy(a)
-
+@multisort(stable=False)
+def quick_sort(a: list[T], key: KeyFunc) -> list[T]:
     quick_sort_helper(a, 0, len(a) - 1, key)
 
     return a
 
 
-@multisort
-def counting_sort(a: list[T], key: KeyFunc):
+@multisort(stable=True)
+def counting_sort(a: list[T], key: KeyFunc) -> list[T]:
     output = copy(a)
 
     max_key: int = key(a[0])
@@ -119,7 +128,6 @@ def counting_sort(a: list[T], key: KeyFunc):
 
     min_key: int = max_key
 
-    #  iterate over
     for v in a[1:]:
         v_key = key(v)
         if type(v_key) is not int:
